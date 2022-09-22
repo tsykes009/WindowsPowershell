60cc9fe0263dac27b78c1169ff7775956dcb2717
Travis Sykes <travis.sykes@pwc.com>
2022-09-22 13:49:55 -0400

    init push

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,18 @@
+Write-Host "PowerShell Version:`t$($PSVersionTable.PSVersion)" -ForegroundColor Green
+Write-Host "PowerShell Edition:`t$($PSVersionTable.PSEdition)" -ForegroundColor Green
+Write-Host "PowerShell Build:`t$($PSVersionTable.BuildVersion)" -ForegroundColor Green
+Write-Host "PowerShell Remoting:`t$($PSVersionTable.PSRemotingProtocolVersion)" -ForegroundColor Green
+
+# MODULE IMPORTS
+Import-Module -NAME PSReadLine
+Import-Module -Name Terminal-Icons
+Import-Module -Name posh-git
+
+# SETTINGS AND PARAMETERS
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
+
+oh-my-posh init pwsh --config (Join-Path $env:POSH_THEMES_PATH -ChildPath aliens.omp.json) | Invoke-Expression
+
+# ALIAS
